0ece8c43553db9c36294e119f6b3cfa359058afd
xinghongxiang <lifeisalvxing@163.com>
2020-04-10 17:42:41 +0800

    yanyan

---
+++ personal/src/main/java/com/yanyan/personal/dao/yanyanMapper.java
@@ -0,0 +1,8 @@
+package com.yanyan.personal.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface yanyanMapper {
+    List<Map<String,Object>> selectSalary();
+}
---
+++ personal/src/main/java/com/yanyan/personal/service/yanyanService.java
@@ -0,0 +1,8 @@
+package com.yanyan.personal.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface yanyanService {
+    List<Map<String, Object>> getSalary();
+}
